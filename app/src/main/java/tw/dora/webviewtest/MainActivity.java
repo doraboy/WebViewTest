2e00f1a9a9b199b35e027c71bcaa3e1e44daf5d4
kleinerQ <chih.yen5408@gmail.com>
2018-10-27 15:59:29 +0800

    v2

--- app/src/main/java/tw/dora/webviewtest/MainActivity.java
+++ app/src/main/java/tw/dora/webviewtest/MainActivity.java
@@ -2,12 +2,26 @@ package tw.dora.webviewtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        initWebView();
     }
+
+    private void initWebView(){
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        //linux系統根路徑為一根/,外加本機檔案通訊協定file://
+        webView.loadUrl("file:///android_asset/brad01.html");
+    }
+
+
 }
